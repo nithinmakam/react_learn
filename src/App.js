1be06124dd13392430ed0089bd762254c0df25b1
nithin <35481108+nithinmakam@users.noreply.github.com>
2022-11-19 22:05:04 +0530

    Added greetings

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'Nithin Makam';
-  return <div>Hello {value}</div>;
+  return <div>Hello Everyone. I am {value}</div>;
 }
 
 export default App;
